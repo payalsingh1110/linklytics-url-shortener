ef33c56fec3076915e69b079f0b11dd315dd40f5
payalsingh1110 <payalsingh.111020@gmail.com>
2025-08-25 01:30:52 +0530

    feat: create models and db setup

---
+++ src/main/java/com/url/url_shortner_sb/models/ClickEvent.java
@@ -0,0 +1,23 @@
+package com.url.url_shortner_sb.models;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Data
+public class ClickEvent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private LocalDateTime clickDate;
+
+    @ManyToOne
+    @JoinColumn(name = "url_mapping_id")
+    private UrlMapping urlMapping;
+
+}
